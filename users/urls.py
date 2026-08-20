b32221eeab5ffa81e33f86501fd6863c6e2761fb
Leodenes <Leodenesv@gmail.com>
2026-08-20 15:35:14 -0300

    Part IV

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
    
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register if hasattr(views, 'register') else auth_views.LoginView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
 ]
